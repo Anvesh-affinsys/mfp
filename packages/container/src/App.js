27125b1d7c75e6bcf271d03cfe2f28ab46a073ee
Anvesh-affinsys <94594268+Anvesh-affinsys@users.noreply.github.com>
2022-06-08 17:33:02 +0530

    container yml changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,10 +3,10 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-    <>
-      <h1>Hi there!</h1>
+    <div>
+      <h1>Hi there!!!!!!!!!!</h1>
       <hr />
       <MarketingApp />
-    </>
+    </div>
   );
 };
