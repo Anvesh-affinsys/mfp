11bc0050099b1d81a5a8a5fb0a5009f95763f173
Anvesh-affinsys <94594268+Anvesh-affinsys@users.noreply.github.com>
2022-06-09 12:21:08 +0530

    change Test in marketing

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page2
+              Landing Page
             </Typography>
             <Typography
               variant="h5"
