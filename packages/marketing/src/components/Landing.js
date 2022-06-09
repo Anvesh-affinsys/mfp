1f80460242496e06225b589fbbf4409c9ec7d78e
Anvesh-affinsys <94594268+Anvesh-affinsys@users.noreply.github.com>
2022-06-09 11:51:04 +0530

    container yml changes

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Landing `Page
+              LandingPage
             </Typography>
             <Typography
               variant="h5"
